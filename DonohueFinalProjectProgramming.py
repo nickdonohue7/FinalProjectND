3fcc71484905b9b8edb27a049121c35e1c979061
nickdonohue7 <120525219+nickdonohue7@users.noreply.github.com>
2022-12-13 21:28:50 -0500

    Add files via upload

---
+++ DonohueFinalProjectProgramming.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import streamlit as st
+
+
+
+s= pd.read_csv("C:/Users/NickDonohue/Downloads/social_media_usage.csv")
+
+
+import pandas as pd
+import numpy as np
+import altair as alt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+
+z= pd.DataFrame ({
+   'Column 1':[1,0,0], 
+    'Column 2':[0,0,1]
+})
+
+print (z)
+
+import numpy as np
+def clean_sm (x):
+    clean_sm= np.where(x==1,
+                             1,
+                             0)
+    return (x)
+
+clean_sm(z)
+
+ss= pd.DataFrame({
+    'sm_li':np.where(s["web1h"]==1,1,0),
+    'income':np.where(s["income"]<=9, s["income"],np.nan),
+    'education':np.where(s["educ2"]<=8, s["educ2"],np.nan),
+    'parent':np.where(s["par"]==1,1,0),
+    'martial':np.where(s["marital"]==1,1,0),
+    'female':np.where(s["gender"]==2,1,0),
+    'age':np.where(s["age"]<=98, s["age"],np.nan)
+    
+})
+
+ss=ss.dropna()
+
+ss= ss.astype(int)
+
+y = ss["sm_li"]
+X = ss[["age", "female", "martial", "parent","education","income"]]
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    stratify=y,       
+                                                    test_size=0.2,   
+                                                    random_state=987)
+
+lr = LogisticRegression(class_weight='balanced')
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_test)
+confusion_matrix(y_test, y_pred)
+
+pd.DataFrame(confusion_matrix(y_test, y_pred),
+           columns=["Predicted negative", "Predicted positive"],
+           index=["Actual negative","Actual positive"]).style.background_gradient(cmap="PiYG")
+
+print(classification_report(y_test, y_pred))
+
+
+st.header("Predicting if you are a LinkedIn user")
+
+user_age=st.slider("What is your age", min_value=10, max_value=97,value= 18, step=1)
+user_gender= st.selectbox("What is your gender? If female select 1, if male select 0",options=[1,0])
+user_married= st.selectbox("Are you married? If so select 1, if not select 0",options=[1,0])
+user_parent= st.selectbox("Are you a parent? If so select 1, if not select 0",options=[1,0])
+st.markdown("What is your education. 1= Less than High School, 2= HS incomplete, 3=HS diploma, 4= some college, 5= two year college, 6= 4 year college, 7= post graduate schooling, 8=PHD")
+user_education= st.slider("What is your highest education", min_value=1, max_value=8,value= 1, step=1)
+st.markdown("What is your income 1= Less than 10,000 2= 10 to under 20,000 3= 20 to under 30,000 4= 30 to under 40,000 5= 40 to under 50,000 6=50 to under 60,000 7=70 to under 100,000 8=100 to under 150,000 9= more than 150,000")
+user_income= st.slider("What is your income", min_value=1, max_value=9,value= 1, step=1)
+
+person = [user_income,user_education,user_parent,user_married,user_gender,user_age]
+
+predicted_class = lr.predict([person])
+
+probs = lr.predict_proba([person])
+
+st.subheader('Is this person a Linkedin user?')
+if predicted_class > 0:
+    label= "This is a Linkedin user!"
+else:
+    label= "This is not a Linkedin user!"
+st.write(label)
+    
+st.subheader('What is the probaility that this person is a Linkedin user')
+st.write(probs)
